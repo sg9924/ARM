d9bdccb61b8a1fcaae3f4f9dc6a6d49abf87c2e4
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-10 12:23:28 +0530

    stm32F407 | Driver Header | NVIC Defintions

--- C/Device Drivers/stm32F407xx.h
+++ C/Device Drivers/stm32F407xx.h
@@ -19,6 +19,26 @@
 
 /********************************************** Generic Macros Definitions End **********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/************************************************** NVIC Definitions Start **************************************************/
 
+// NVIC ISERx Register Addresses
+#define NVIC_ISER0          ((volatile uint32_t*)0xE000E100)
+#define NVIC_ISER1          ((volatile uint32_t*)0xE000E104)
+#define NVIC_ISER2          ((volatile uint32_t*)0xE000E108)
+#define NVIC_ISER3          ((volatile uint32_t*)0xE000E10c)
+
+// NVIC ICERx Register Addresses
+#define NVIC_ICER0 			((volatile uint32_t*)0XE000E180)
+#define NVIC_ICER1			((volatile uint32_t*)0XE000E184)
+#define NVIC_ICER2  		((volatile uint32_t*)0XE000E188)
+#define NVIC_ICER3			((volatile uint32_t*)0XE000E18C)
+
+// NVIC Prority Register Address
+#define NVIC_PR_BASE_ADDR 	((volatile uint32_t*)0xE000E400)
+
+// No. of Priority Bits Implemented
+#define NO_PR_BITS_IMPLEMENTED  4
+/*************************************************** NVIC Definitions End ***************************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 #endif /* INC_STM3F407XX_H */
