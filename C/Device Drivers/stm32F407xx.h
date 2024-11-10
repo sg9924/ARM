87b30bb0f8f504d9717e1bff4d4785c582cd28e5
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-10 12:21:00 +0530

    stm32F407 | Driver Header | Generic Macro Definitions

--- C/Device Drivers/stm32F407xx.h
+++ C/Device Drivers/stm32F407xx.h
@@ -1,6 +1,24 @@
 #ifndef INC_STM3F407XX_H
 #define INC_STM3F407XX_H
+/*--------------------------------------------------------------------------------------------------------------------------*/
+// standard header files
+#include<stddef.h>
+#include<stdint.h>
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/********************************************* Generic Macros Definitions Start *********************************************/
 
+#define ENABLE 				1
+#define DISABLE 			0
+
+#define SET 				ENABLE
+#define RESET 				DISABLE
+#define GPIO_PIN_SET        SET
+#define GPIO_PIN_RESET      RESET
+#define FLAG_RESET          RESET
+#define FLAG_SET 			SET
+
+/********************************************** Generic Macros Definitions End **********************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 
 #endif /* INC_STM3F407XX_H */
