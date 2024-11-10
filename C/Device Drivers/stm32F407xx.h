3698d1f8e45905276acba54ef90156cfc0132b97
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-10 12:26:03 +0530

    stm32F407 | Driver Header | Memory Address Definitions

--- C/Device Drivers/stm32F407xx.h
+++ C/Device Drivers/stm32F407xx.h
@@ -69,7 +69,85 @@
 #define NVIC_IRQ_PRI15          15
 /**************************************************** IRQ Definitions End ***************************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/********************************************* Memory Address Definitions Start *********************************************/
+// Memory Base Addresses
+#define FLASH_BASE_ADDR     0x08000000U
+#define SRAM1_BASE_ADDR     0x20000000U
+#define SRAM2_BASE_ADDR     0x2001C000U
+#define SRAM_BASE_ADDR      SRAM1_BASE_ADDR
 
+
+// Peripheral Base Address
+#define PERIP_BASE_ADDR     0x40000000U
+
+// Bus Domain Bases Addresses
+#define APB1_BASE_ADDR      PERIP_BASE_ADDR
+#define APB2_BASE_ADDR      0x40010000U
+#define AHB1_BASE_ADDR      0x40020000U
+#define AHB2_BASE_ADDR      0x50000000U
+
+
+// AHB1 Peripherals Base Addresses
+// GPIO Base Addresses
+#define GPIOA_BASE_ADDR     (AHB1_BASE_ADDR) 
+#define GPIOB_BASE_ADDR     (AHB1_BASE_ADDR + 0x0400)
+#define GPIOC_BASE_ADDR     (AHB1_BASE_ADDR + 0x0800)
+#define GPIOD_BASE_ADDR     (AHB1_BASE_ADDR + 0x0C00)
+#define GPIOE_BASE_ADDR     (AHB1_BASE_ADDR + 0x1000)
+#define GPIOF_BASE_ADDR     (AHB1_BASE_ADDR + 0x1400)
+#define GPIOG_BASE_ADDR     (AHB1_BASE_ADDR + 0x1800)
+#define GPIOH_BASE_ADDR     (AHB1_BASE_ADDR + 0x1C00)
+#define GPIOI_BASE_ADDR     (AHB1_BASE_ADDR + 0x2000)
+#define GPIOJ_BASE_ADDR     (AHB1_BASE_ADDR + 0x2400)
+#define GPIOK_BASE_ADDR     (AHB1_BASE_ADDR + 0x2800)
+
+// CRC
+#define CRC_BASE_ADDR       (AHB1_BASE_ADDR + 0x3000)
+
+// RCC - Reset & Clock Control Base Address
+#define RCC_BASE_ADDR       (AHB1_BASE_ADDR + 0x3800)
+
+
+// APB1 Peripherals Base Addresses
+// SPI
+#define SPI2_BASE_ADDR      (APB1_BASE_ADDR + 0x3800)
+#define SPI3_BASE_ADDR      (APB1_BASE_ADDR + 0x3FFF)
+
+// USART
+#define USART2_BASE_ADDR    (APB1_BASE_ADDR + 0x4400)
+#define USART3_BASE_ADDR    (APB1_BASE_ADDR + 0x4800)
+
+// UART
+#define UART4_BASE_ADDR     (APB1_BASE_ADDR + 0x4C00)
+#define UART5_BASE_ADDR     (APB1_BASE_ADDR + 0x5000)
+#define UART7_BASE_ADDR     (APB1_BASE_ADDR + 0x7800)
+#define UART8_BASE_ADDR     (APB1_BASE_ADDR + 0x7C00)
+
+//I2C
+#define I2C1_BASE_ADDR      (APB1_BASE_ADDR + 0x5400)
+#define I2C2_BASE_ADDR      (APB1_BASE_ADDR + 0x5800)
+#define I2C3_BASE_ADDR      (APB1_BASE_ADDR + 0x5C00)
+
+
+// APB2 Peripherals Base Addresses
+// USART
+#define USART1_BASE_ADDR    (APB2_BASE_ADDR + 0x1000)
+#define USART6_BASE_ADDR    (APB2_BASE_ADDR + 0x1400)
+
+// SPI
+#define SPI1_BASE_ADDR      (APB2_BASE_ADDR + 0x3000)
+#define SPI4_BASE_ADDR      (APB2_BASE_ADDR + 0x3400)
+#define SPI5_BASE_ADDR      (APB2_BASE_ADDR + 0x5000)
+#define SPI6_BASE_ADDR      (APB2_BASE_ADDR + 0x5400)
+
+//SYSCFG
+#define SYSCFG_BASE_ADDR      (APB2_BASE_ADDR + 0x3800)
+
+//EXTI
+#define EXTI_BASE_ADDR      (APB2_BASE_ADDR + 0x3C00)
+
+/********************************************** Memory Address Definitions End **********************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 
 
