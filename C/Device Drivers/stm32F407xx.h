384ab4fd615b8f8e0c4421a52547d17be41257c6
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-10 12:24:27 +0530

    stm32F407 | Driver Header | IRQ Definitions

--- C/Device Drivers/stm32F407xx.h
+++ C/Device Drivers/stm32F407xx.h
@@ -40,5 +40,42 @@
 #define NO_PR_BITS_IMPLEMENTED  4
 /*************************************************** NVIC Definitions End ***************************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/************************************************** IRQ Definitions Start ***************************************************/
+
+// IRQ Numbers
+#define IRQ_NO_EXTI0 		    6
+#define IRQ_NO_EXTI1 		    7
+#define IRQ_NO_EXTI2 		    8
+#define IRQ_NO_EXTI3 		    9
+#define IRQ_NO_EXTI4 		    10
+#define IRQ_NO_EXTI9_5 		    23
+#define IRQ_NO_EXTI15_10 	    40
+#define IRQ_NO_SPI1			    35
+#define IRQ_NO_SPI2             36
+#define IRQ_NO_SPI3             51
+#define IRQ_NO_SPI4
+#define IRQ_NO_I2C1_EV          31
+#define IRQ_NO_I2C1_ER          32
+#define IRQ_NO_USART1	        37
+#define IRQ_NO_USART2	        38
+#define IRQ_NO_USART3	        39
+#define IRQ_NO_UART4	        52
+#define IRQ_NO_UART5	        53
+#define IRQ_NO_USART6	        71
+
+
+// IRQ Priority Levels
+#define NVIC_IRQ_PRI0           0
+#define NVIC_IRQ_PRI15          15
+/**************************************************** IRQ Definitions End ***************************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
+
 
 #endif /* INC_STM3F407XX_H */
