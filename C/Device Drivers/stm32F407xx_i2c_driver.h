e61c1576fdc8ad38a45ad63cde9244005d1f31be
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 20:00:13 +0530

    stm32F407 | I2C Driver Header| Macro Definitions

--- C/Device Drivers/stm32F407xx_i2c_driver.h
+++ C/Device Drivers/stm32F407xx_i2c_driver.h
@@ -36,6 +36,61 @@ typedef struct
 
 /********************************************** I2C Structure Definitions End ***********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/************************************************** I2C Definitions Start ***************************************************/
+
+// I2C States
+#define I2C_READY 					    0
+#define I2C_BUSY_IN_RX 				    1
+#define I2C_BUSY_IN_TX 				    2
+
+// I2C SCL Speeds
+#define I2C_SCL_SPEED_SM 	            100000
+#define I2C_SCL_SPEED_FM4K 	            400000
+#define I2C_SCL_SPEED_FM2K              200000
+
+
+// I2C Ack Control
+#define I2C_ACK_ENABLE                  1
+#define I2C_ACK_DISABLE                 0
+
+
+// I2C FM Duty Cycle
+#define I2C_FM_DUTY_2                   0
+#define I2C_FM_DUTY_16_9                1
+
+
+// I2C Status Flags Configuration Values
+#define I2C_FLAG_TXE   		            (1 << I2C_SR1_TXE)
+#define I2C_FLAG_RXNE   	            (1 << I2C_SR1_RXNE)
+#define I2C_FLAG_SB			            (1 << I2C_SR1_SB)
+#define I2C_FLAG_OVR  		            (1 << I2C_SR1_OVR)
+#define I2C_FLAG_AF   		            (1 << I2C_SR1_AF)
+#define I2C_FLAG_ARLO 		            (1 << I2C_SR1_ARLO)
+#define I2C_FLAG_BERR 		            (1 << I2C_SR1_BERR)
+#define I2C_FLAG_STOPF 		            (1 << I2C_SR1_STOPF)
+#define I2C_FLAG_ADD10 		            (1 << I2C_SR1_ADD10)
+#define I2C_FLAG_BTF  		            (1 << I2C_SR1_BTF)
+#define I2C_FLAG_ADDR 		            (1 << I2C_SR1_ADDR)
+#define I2C_FLAG_TIMEOUT 	            (1 << I2C_SR1_TIMEOUT)
+
+#define I2C_DISABLE_SR  	            RESET
+#define I2C_ENABLE_SR   	            SET
+
+
+// I2C Events Definitions
+#define I2C_EV_TX_CMPLT  	 	        0
+#define I2C_EV_RX_CMPLT  	 	        1
+#define I2C_EV_STOP       		        2
+#define I2C_ERROR_BERR 	 		        3
+#define I2C_ERROR_ARLO  		        4
+#define I2C_ERROR_AF    		        5
+#define I2C_ERROR_OVR   		        6
+#define I2C_ERROR_TIMEOUT 		        7
+#define I2C_EV_DATA_REQ                 8
+#define I2C_EV_DATA_RCV                 9
+
+/*************************************************** I2C Definitions End ****************************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 
 #endif /*INC_STM32F407XX_I2C_DRIVER_H*/
