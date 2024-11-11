6ed495c88b3415f9d8f5074a5a98375118558f78
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 20:37:30 +0530

    stm32F407 | RCC Driver Header | Function Declarations  (2)

--- C/Device Drivers/stm32F407xx_rcc_driver.h
+++ C/Device Drivers/stm32F407xx_rcc_driver.h
@@ -10,8 +10,6 @@ uint32_t RCC_GetPCLK1Value(void);
 //This returns the APB2 clock value
 uint32_t RCC_GetPCLK2Value(void);
 
-
-uint32_t  RCC_GetPLLOutputClock(void);
 #endif /* INC_STM32F407XX_RCC_DRIVER_H_ */
 /********************************************** RCC Function Declarations End ***********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
