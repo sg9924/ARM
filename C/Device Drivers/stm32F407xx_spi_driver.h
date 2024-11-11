7e4f8ec1cb4ec2ac3c5bf085c28bba9984747cc6
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 06:17:35 +0530

    stm32F407 | SPI Driver Header | Function Declarations

--- C/Device Drivers/stm32F407xx_spi_driver.h
+++ C/Device Drivers/stm32F407xx_spi_driver.h
@@ -33,6 +33,42 @@ typedef struct
 }SPI_Handle;
 /*********************************************** SPI Structure Definitions End **********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/*********************************************** SPI API's Declarartions Start **********************************************/
 
+// SPI Peripheral Clock Enable
+void SPI_Clk_Enable(SPI_RegDef *pSPIx, uint8_t EnorDi);
+
+// Init and De-init
+void SPI_Init(SPI_Handle *pSPIHandle);
+void SPI_DeInit(SPI_RegDef *pSPIx);
+
+// Data Send & Receive
+void SPI_SendData(SPI_RegDef *pSPIx,uint8_t *pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef *pSPIx, uint8_t *pRxBuffer, uint32_t Len);
+
+// Data Send & Receive for Interrupts
+uint8_t SPI_SendDataIT(SPI_Handle *pSPIHandle,uint8_t *pTxBuffer, uint32_t Len);
+uint8_t SPI_ReceiveDataIT(SPI_Handle *pSPIHandle, uint8_t *pRxBuffer, uint32_t Len);
+
+// IRQ Configuration and ISR handling
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle *pHandle);
+
+// Peripheral Control Functions
+void SPI_Enable(SPI_RegDef *pSPIx, uint8_t mode);
+void SPI_SSIConfig(SPI_RegDef *pSPIx, uint8_t EnOrDi);
+void SPI_SSOEConfig(SPI_RegDef *pSPIx, uint8_t EnOrDi);
+uint8_t SPI_GetFlagStatus(SPI_RegDef *pSPIx , uint32_t FlagName);
+void SPI_ClearOVRFlag(SPI_RegDef *pSPIx);
+void SPI_CloseTransmisson(SPI_Handle *pSPIHandle);
+void SPI_CloseReception(SPI_Handle *pSPIHandle);
+// uint8_t I2C_DeviceMode(I2C_RegDef *I2Cx); //--> ?????
+
+// Application callback
+void SPI_ApplicationEventCallback(SPI_Handle *pSPIHandle, uint8_t AppEv);
+
+/************************************************ SPI API's Declarations End ************************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 #endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
