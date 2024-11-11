697ef06bbd99296e9e901da9cd5da3a092610066
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 20:17:44 +0530

    stm32F407 | I2C Driver Program | Functions Definitions (4)
    I2C Interrupt Configuration
    I2C Interrupt Priority Configuration

--- C/Device Drivers/stm32F407xx_i2c_driver.c
+++ C/Device Drivers/stm32F407xx_i2c_driver.c
@@ -351,3 +351,58 @@ void I2C_MasterReceiveData(I2C_Handle *pI2CHandle,uint8_t *pRxBuffer, uint8_t Le
 	}
 
 }
+
+// I2C - IRQ Interrupt Config
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t mode)
+{
+
+	if(mode == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ISER0 register
+			*NVIC_ISER0 |= (1<<IRQNumber);
+
+		}else if(IRQNumber > 31 && IRQNumber < 64) //32 to 63
+		{
+			//program ISER1 register
+			*NVIC_ISER1 |= (1<<(IRQNumber % 32));
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96)
+		{
+			//program ISER2 register //64 to 95
+			*NVIC_ISER3 |= (1<<(IRQNumber % 64));
+		}
+	}else
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ICER0 register
+			*NVIC_ICER0 |= (1<<IRQNumber);
+		}else if(IRQNumber > 31 && IRQNumber < 64)
+		{
+			//program ICER1 register
+			*NVIC_ICER1 |= (1<<(IRQNumber % 32));
+		}
+		else if(IRQNumber >= 6 && IRQNumber < 96)
+		{
+			//program ICER2 register
+			*NVIC_ICER3 |= (1<<(IRQNumber % 64));
+		}
+	}
+
+}
+
+
+// I2C - IRQ Priority Config
+void I2C_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
+{
+	// Locate IPR Register
+	uint8_t iprx = IRQNumber / 4; // locate the block
+	uint8_t iprx_section = IRQNumber %4; // locate the register section
+
+	uint8_t shift_amount = (8 * iprx_section) + (8 - NO_PR_BITS_IMPLEMENTED); // configure the value
+
+	*(NVIC_PR_BASE_ADDR + iprx) |=  (IRQPriority<<shift_amount); // load the value into the register setion
+
+}
