0d3237b947813b8fec74927db7e358e3d1f78009
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 06:38:37 +0530

    stm32F407 | SPI Driver Header | SPI Macros

--- C/Device Drivers/stm32F407xx_spi_driver.h
+++ C/Device Drivers/stm32F407xx_spi_driver.h
@@ -33,6 +33,61 @@ typedef struct
 }SPI_Handle;
 /*********************************************** SPI Structure Definitions End **********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/************************************************** SPI Definitions Start ***************************************************/
+
+// SPI Application States
+#define SPI_READY 					        0           /*<>*/
+#define SPI_BUSY_IN_RX 				        1           /*<>*/
+#define SPI_BUSY_IN_TX 				        2           /*<>*/
+
+// Possible SPI Application Events
+#define SPI_EVENT_TX_CMPLT                  1           /*<>*/
+#define SPI_EVENT_RX_CMPLT                  2           /*<>*/
+#define SPI_EVENT_OVR_ERR                   3           /*<>*/
+#define SPI_EVENT_CRC_ERR                   4           /*<>*/
+
+// SPI Device Mode
+#define SPI_DEVICE_MODE_MASTER              1           /*<>*/
+#define SPI_DEVICE_MODE_SLAVE               0           /*<>*/
+
+// SPI Bus Config
+#define SPI_BUS_CONFIG_FD                   1           /*<>*/
+#define SPI_BUS_CONFIG_HD                   2           /*<>*/
+#define SPI_BUS_CONFIG_SIMPLEX_RXONLY       3           /*<>*/
+
+// SPI Serial Clock Speed
+#define SPI_SCLK_SPEED_DIV2             	0           /*<>*/
+#define SPI_SCLK_SPEED_DIV4             	1           /*<>*/
+#define SPI_SCLK_SPEED_DIV8             	2           /*<>*/
+#define SPI_SCLK_SPEED_DIV16             	3           /*<>*/
+#define SPI_SCLK_SPEED_DIV32             	4           /*<>*/
+#define SPI_SCLK_SPEED_DIV64             	5           /*<>*/
+#define SPI_SCLK_SPEED_DIV128             	6           /*<>*/
+#define SPI_SCLK_SPEED_DIV256             	7           /*<>*/
+
+// SPI Data Frame Format
+#define SPI_DFF_8BITS 	                    0           /*<>*/
+#define SPI_DFF_16BITS                      1           /*<>*/
+
+// SPI CLock Polarity
+#define SPI_CPOL_HIGH                       1           /*<>*/
+#define SPI_CPOL_LOW                        0           /*<>*/
+
+// SPI Clock Phase
+#define SPI_CPHA_HIGH                       1           /*<>*/
+#define SPI_CPHA_LOW                        0           /*<>*/
+
+// SPI Software Slave Management
+#define SPI_SSM_EN                          1           /*<>*/
+#define SPI_SSM_DI                          0           /*<>*/
+
+// SPI related status flags definitions
+#define SPI_TXE_FLAG            (1 << SPI_SR_TXE)       /*<>*/
+#define SPI_RXNE_FLAG           (1 << SPI_SR_RXNE)      /*<>*/
+#define SPI_BUSY_FLAG           (1 << SPI_SR_BSY)       /*<>*/
+
+/**************************************************** SPI Definitions End ***************************************************/
+/*--------------------------------------------------------------------------------------------------------------------------*/
 /*********************************************** SPI API's Declarartions Start **********************************************/
 
 // SPI Peripheral Clock Enable
