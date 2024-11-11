c1486fd4335afe87494d7527c73ef744bdccec55
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-11 20:13:54 +0530

    stm32F407 | I2C Driver Program | Functions Definitions (2)
    I2C Peripheral Enable
    I2C Clock Enable
    I2C Initialization

--- C/Device Drivers/stm32F407xx_i2c_driver.c
+++ C/Device Drivers/stm32F407xx_i2c_driver.c
@@ -105,3 +105,111 @@ void I2C_SlaveEnableDisableCallbackEvents(I2C_RegDef *pI2Cx,uint8_t mode)
 
 /****************************************** I2C Helper Functions Definitions End ********************************************/
 /*--------------------------------------------------------------------------------------------------------------------------*/
+/*********************************************** I2C API's Definitions Start ************************************************/
+
+// I2C - Enable I2C Peripheral
+void I2C_PeripheralControl(I2C_RegDef *pI2Cx, uint8_t mode)
+{
+	if(mode == ENABLE)
+	{
+		pI2Cx->CR1 |= (1<<I2C_CR1_PE);
+		//pI2cBaseAddress->CR1 |= I2C_CR1_PE_Bit_Mask;
+	}else
+	{
+		pI2Cx->CR1 &= ~(1<<0);
+	}
+
+}
+
+
+// I2C - Enable Clock for I2C Peripheral
+void I2C_ClkEnable(I2C_RegDef *pI2Cx, uint8_t mode)
+{
+	if(mode == ENABLE)
+	{
+		if(pI2Cx == I2C1)
+		{
+			I2C1_PCLK_EN();
+		}else if (pI2Cx == I2C2)
+		{
+			I2C2_PCLK_EN();
+		}else if (pI2Cx == I2C3)
+		{
+			I2C3_PCLK_EN();
+		}
+	}
+	else
+	{
+		//TODO
+	}
+
+}
+
+
+// I2C - Initalization
+void I2C_Init(I2C_Handle *pI2CHandle)
+{
+	uint32_t tempreg = 0;
+
+	//Enable Clock for I2C
+	I2C_ClkEnable(pI2CHandle->pI2Cx,ENABLE);
+
+	//Set ACK control Bit (Enable Acknowledging)
+	tempreg |= pI2CHandle->I2C_Config.I2C_AckControl<<10;
+	pI2CHandle->pI2Cx->CR1 = tempreg;
+
+	//Configure FREQ
+	tempreg = 0;
+	tempreg |= RCC_GetPCLK1Value() /1000000U; // Get Configuration value for the Clock Frequency
+	pI2CHandle->pI2Cx->CR2 = (tempreg & 0x3F); // load the configurated value into I2C -> CR2
+
+   //Configure the Host Device Address
+	tempreg = 0;
+	tempreg |= pI2CHandle->I2C_Config.I2C_DeviceAddress<<1;
+	tempreg |= (1<<14);
+	pI2CHandle->pI2Cx->OAR1 = tempreg;
+
+	//CCR calculations
+	uint16_t ccr_value = 0;
+	tempreg = 0;
+
+	if(pI2CHandle->I2C_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_SM)  //For Standard Mode
+	{
+		ccr_value = (RCC_GetPCLK1Value() / (2 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		tempreg |= (ccr_value & 0xFFF);
+	}
+    else //For Fast Mode
+	{
+		tempreg |= (1<<15);
+		tempreg |= (pI2CHandle->I2C_Config.I2C_FMDutyCycle<<14);
+		if(pI2CHandle->I2C_Config.I2C_FMDutyCycle == I2C_FM_DUTY_2)
+		{
+			ccr_value = (RCC_GetPCLK1Value() / (3 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		}else
+		{
+			ccr_value = (RCC_GetPCLK1Value() / (25 * pI2CHandle->I2C_Config.I2C_SCLSpeed));
+		}
+		tempreg |= (ccr_value & 0xFFF);
+	}
+	pI2CHandle->pI2Cx->CCR = tempreg;
+
+	//TRISE Configuration
+	if(pI2CHandle->I2C_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_SM) //For Standard Mode
+	{
+
+		tempreg = (RCC_GetPCLK1Value() /1000000U) + 1;
+	}
+    else //For Fast Mode
+	{
+		tempreg = ((RCC_GetPCLK1Value() * 300) / 1000000000U) + 1;
+	}
+
+	pI2CHandle->pI2Cx->TRISE = (tempreg & 0x3F);
+}
+
+
+// I2C - Deintialization
+void I2C_DeInit(I2C_RegDef *pI2Cx)
+{
+    //TODO
+}
