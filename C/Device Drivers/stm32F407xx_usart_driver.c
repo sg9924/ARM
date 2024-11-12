368a14e8ee63b986ef62c29b677c54baa7071e49
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-12 11:23:14 +0530

    stm32F407 | USART Driver Program | Functions Definitions (4)
    USART Interrupt Config and Interrupt Priority Config
    USART Clear Flag

--- C/Device Drivers/stm32F407xx_usart_driver.c
+++ C/Device Drivers/stm32F407xx_usart_driver.c
@@ -326,10 +326,10 @@ uint8_t USART_SendDataIT(USART_Handle *pUSARTHandle,uint8_t *pTxBuffer, uint32_t
 		pUSARTHandle->TxBusyState = USART_BUSY_IN_TX; //set state as busy in TX
 
 		//Enable interrupt for TXE
-		pUSARTHandle->pUSARTx->CR1 |= ( 1 << USART_CR1_TXEIE);
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_TXEIE);
 
 		//Enable interrupt for TC
-		pUSARTHandle->pUSARTx->CR1 |= ( 1 << USART_CR1_TCIE); 
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_TCIE); 
 	}
 
 	return txstate;
@@ -351,12 +351,75 @@ uint8_t USART_ReceiveDataIT(USART_Handle *pUSARTHandle,uint8_t *pRxBuffer, uint3
 		(void)pUSARTHandle->pUSARTx->DR;
 
 		//Enable interrupt for RXNE
-		pUSARTHandle->pUSARTx->CR1 |= ( 1 << USART_CR1_RXNEIE);
+		pUSARTHandle->pUSARTx->CR1 |= (1 << USART_CR1_RXNEIE);
 	}
 
 	return rxstate;
 }
 
+
+// USART - Clear Flag
+void USART_ClearFlag(USART_RegDef *pUSARTx, uint16_t StatusFlagName)
+{
+	pUSARTx->SR &= ~(StatusFlagName);
+}
+
+
+// USART Interrupt Configuration
+void USART_IRQInterruptConfig(uint8_t IRQNumber, uint8_t mode)
+{
+
+	if(mode == ENABLE) //interrupt enable
+	{
+		if(IRQNumber <= 31) //for IRQ number equal to or below 31
+		{
+			//program ISER0 register
+			*NVIC_ISER0 |= (1 << IRQNumber);
+
+		}else if(IRQNumber > 31 && IRQNumber < 64) //32 to 63
+		{
+			//program ISER1 register
+			*NVIC_ISER1 |= (1 << (IRQNumber % 32));
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96) //64-95
+		{
+			//program ISER2 register //64 to 95
+			*NVIC_ISER3 |= (1 << (IRQNumber % 64));
+		}
+	}
+    else //interrupt disable
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ICER0 register
+			*NVIC_ICER0 |= (1 << IRQNumber);
+		}
+        else if(IRQNumber > 31 && IRQNumber < 64)
+		{
+			//program ICER1 register
+			*NVIC_ICER1 |= (1 << (IRQNumber % 32));
+		}
+		else if(IRQNumber >= 6 && IRQNumber < 96)
+		{
+			//program ICER2 register
+			*NVIC_ICER3 |= (1 << (IRQNumber % 64));
+		}
+	}
+}
+
+
+// USART - Interrupt Priority Configuration
+void USART_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
+{
+	//Locate IPR Register
+	uint8_t iprx = IRQNumber/4; //Locate the block
+	uint8_t iprx_section = IRQNumber %4; //Locate the register section
+
+	uint8_t shift_amount = (8 * iprx_section) + (8 - NO_PR_BITS_IMPLEMENTED) ;
+
+	*(NVIC_PR_BASE_ADDR + iprx) |= (IRQPriority << shift_amount);
+
+}
 
 
 
