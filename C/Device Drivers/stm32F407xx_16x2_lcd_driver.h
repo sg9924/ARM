d2c07c2815d8592a944450b4663878d5ac832653
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-12 12:42:55 +0530

    stm32F407 | 16x2 LCD Driver Header | Initial

---
+++ C/Device Drivers/stm32F407xx_16x2_lcd_driver.h
@@ -0,0 +1,35 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include "stm32F407xx.h"
+
+// Macros
+#define LCD_GPIO_PORT                   GPIOD
+#define LCD_GPIO_RS                     GPIO_PIN0
+#define LCD_GPIO_RW                     GPIO_PIN1
+#define LCD_GPIO_EN                     GPIO_PIN2
+#define LCD_GPIO_D0                     GPIO_PIN3
+#define LCD_GPIO_D1                     GPIO_PIN4
+#define LCD_GPIO_D2                     GPIO_PIN5
+#define LCD_GPIO_D3                     GPIO_PIN6
+#define LCD_GPIO_D4                     GPIO_PIN7
+#define LCD_GPIO_D5                     GPIO_PIN8
+#define LCD_GPIO_D6                     GPIO_PIN9
+#define LCD_GPIO_D7                     GPIO_PIN10
+
+
+// LCD Commands Macros
+#define LCD_CMD_4DL_2N_5X8F             0x28
+#define LCD_CMD_DON_CURON               0x0E
+#define LCD_CMD_INCADD                  0x06
+#define LCD_CMD_DIS_CLEAR               0x01
+#define LCD_CMD_DIS_RETURN_HOME         0x02      
+
+
+// LCD Functions Declarations
+void lcd_init(void);
+void lcd_send_command(uint8_t);
+void lcd_send_char(uint8_t);
+void lcd_send_string(char*);
+
+#endif /*LCD_H*/
