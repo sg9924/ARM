61fdc7be31b79b54e9943d9ccd1a50d07b674a37
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-11-12 14:02:44 +0530

    stm32F407 | 16x2 LCD Driver Program | Functions Definitions (2)
    16x2 LCD Initialization

--- C/Device Drivers/stm32F407xx_16x2_lcd_driver.c
+++ C/Device Drivers/stm32F407xx_16x2_lcd_driver.c
@@ -85,7 +85,91 @@ void lcd_send_string(char* msg)
 
 
 
+void lcd_init()
+{
+    //1. Configure GPIO pins used for LCD Connections
+    GPIO_Handle lcd_signal;
 
+    lcd_signal.pGPIOx = LCD_GPIO_PORT;
+    lcd_signal.GPIOx_PinConfig.PinMode = GPIO_MODE_OP;
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_RS;
+    lcd_signal.GPIOx_PinConfig.PinOPType = GPIO_OP_TYPE_PP;
+    lcd_signal.GPIOx_PinConfig.PinPUPDCtrl = GPIO_NO_PUPD;
+    lcd_signal.GPIOx_PinConfig.PinSpeed = GPIO_OP_SPEED_FAST;
+    GPIO_Init(&lcd_signal);
+
+    //2. Intialize GPIO Pins for the respectiveLCD Pins
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_RW;
+    GPIO_Init(&lcd_signal);
+
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_EN;
+    GPIO_Init(&lcd_signal);
+
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_D4;
+    GPIO_Init(&lcd_signal);
+
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_D5;
+    GPIO_Init(&lcd_signal);
+
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_D6;
+    GPIO_Init(&lcd_signal);
+
+    lcd_signal.GPIOx_PinConfig.PinNo = LCD_GPIO_D7;
+    GPIO_Init(&lcd_signal);
+
+
+    //3. Set initial value as 0 for all the pins configured for the LCD
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_EN, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_D4, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_D5, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_D6, GPIO_PIN_RESET);
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_D7, GPIO_PIN_RESET);
+
+
+    //4. Initialize the LCD
+    //provide 15ms delay
+    mdelay(15);
+
+    //Function set
+    write_bits_4(0x3);
+
+    //a. set 0 to RS & RW
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET); 
+    GPIO_WriteOpPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+
+    //provide 4.1 ms (5 ms) delay
+    mdelay(5);
+    
+    //Function set
+    write_bits_4(0x3);
+
+    //provide 100 us delay
+    udelay(100);
+
+    //Function set
+    write_bits_4(0x3);
+
+    //Function set
+    write_bits_4(0x3);
+    //Function set
+    write_bits_4(0x2);
+
+    //function set command - 4 data lines, 2 display lines, small font
+    lcd_send_command(LCD_CMD_4DL_2N_5X8F);
+
+    //display on, cursor on
+    lcd_send_command(LCD_CMD_DON_CURON);
+
+    //display clear
+    lcd_send_command(LCD_CMD_DIS_CLEAR);
+    mdelay(2);//provide delay - 2ms
+
+    //entry mode set
+    lcd_send_command(LCD_CMD_INCADD);
+
+}
 
 
 /*--------------------------------------------------------------------------------------------------------------------------*/
