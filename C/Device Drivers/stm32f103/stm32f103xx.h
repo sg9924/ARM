5dd4767ee6bf32b7c6ad12d3627c5a05c5585e38
sg9924 <sg9924@users.noreply.github.com>
2025-02-09 17:31:57 +0530

    stm32f103 | Device Header File | Formatting

--- C/Device Drivers/stm32f103/stm32f103xx.h
+++ C/Device Drivers/stm32f103/stm32f103xx.h
@@ -9,15 +9,15 @@
 /*--------------------------------------------------------------------------------------------------------------------------*/
 /********************************************* Generic Macros Definitions Start *********************************************/
 
-#define ENABLE 				1
-#define DISABLE 			0
+#define ENABLE                 1
+#define DISABLE             0
 
-#define SET 				ENABLE
-#define RESET 				DISABLE
+#define SET                 ENABLE
+#define RESET                 DISABLE
 #define GPIO_PIN_SET        SET
 #define GPIO_PIN_RESET      RESET
 #define FLAG_RESET          RESET
-#define FLAG_SET 			SET
+#define FLAG_SET             SET
 
 // Bit Manipulations !!Currently not used!!
 #define READ_BIT(reg,bit_field)        (reg>>bit_field)
