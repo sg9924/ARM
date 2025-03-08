a940cb271eac1a952801b72157c615b59d16b2f1
sg9924 <sg9924@users.noreply.github.com>
2025-03-08 19:00:13 +0530

    stm32f103 | 7 Seg Header | Model Specific Header Files
    Header Files containing pin numbers for the segments based on the model

---
+++ C/Device Drivers/stm32f103/seven_seg_5161AS.h
@@ -0,0 +1,15 @@
+#ifndef SEG5161AS_H
+#define SEG5161AS_H
+
+// Segment pin numbers for the model 5161AS
+#define SEG_A        SEG_PIN7
+#define SEG_B        SEG_PIN6
+#define SEG_C        SEG_PIN4
+#define SEG_D        SEG_PIN2
+#define SEG_E        SEG_PIN1
+#define SEG_F        SEG_PIN9
+#define SEG_G        SEG_PIN10
+#define SEG_H        SEG_PIN5
+
+
+#endif /*SEG5161AS_H*/
---
+++ C/Device Drivers/stm32f103/seven_seg_5631AS.h
@@ -0,0 +1,15 @@
+#ifndef SEG5631AS_H
+#define SEG5631AS_H
+
+// Segment pin numbers for the model 5631AS
+#define SEG_A        SEG_PIN11
+#define SEG_B        SEG_PIN7
+#define SEG_C        SEG_PIN4
+#define SEG_D        SEG_PIN2
+#define SEG_E        SEG_PIN1
+#define SEG_F        SEG_PIN10
+#define SEG_G        SEG_PIN5
+#define SEG_H        SEG_PIN3
+
+
+#endif /*SEG5631AS_H*/
