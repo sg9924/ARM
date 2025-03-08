b744addf3e71165227e2ef6d12c9128ca8e52ef8
sg9924 <sg9924@users.noreply.github.com>
2025-03-08 19:02:46 +0530

    stm32f103 | 7 Seg GPIO Map Header | Initial
    GPIO Pin no to 7 Seg pin no Mapping

---
+++ C/Device Drivers/stm32f103/gpio_7_seg_map.h
@@ -0,0 +1,26 @@
+#ifndef GPIO7SEGMAP_H
+#define GPIO7SEGMAP_H
+
+//Mapping of GPIO pins to SEG pins
+/* for 5631AS
+#define SEG_PIN1     GPIO_PIN4
+#define SEG_PIN2     GPIO_PIN3
+#define SEG_PIN3     GPIO_PIN2
+#define SEG_PIN4     GPIO_PIN1
+#define SEG_PIN5     GPIO_PIN0
+#define SEG_PIN7     GPIO_PIN7
+#define SEG_PIN10    GPIO_PIN8
+#define SEG_PIN11    GPIO_PIN9
+*/
+
+// for 5161AS
+#define SEG_PIN1     GPIO_PIN3
+#define SEG_PIN2     GPIO_PIN2
+#define SEG_PIN4     GPIO_PIN1
+#define SEG_PIN5     GPIO_PIN0
+#define SEG_PIN6     GPIO_PIN4
+#define SEG_PIN7     GPIO_PIN7
+#define SEG_PIN9     GPIO_PIN8
+#define SEG_PIN10    GPIO_PIN9
+
+#endif /*GPIO7SEGMAP_H*/
