569238be04c8d4cb9dc863dac1b2017da798b642
sg9924 <sg9924@users.noreply.github.com>
2025-06-16 21:47:47 +0530

    stm32f103 | Programs | LED Toggle

---
+++ C/Programs/stm32f103/LED_Toggle.c
@@ -0,0 +1,20 @@
+#include"stm32f103xx_init.h"
+#include"stm32f103xx_gpio.h"
+
+
+int main()
+{
+    board_init();
+
+    GPIO_Handle led;
+    GPIO_Config(&led, GPIOC, GPIO_MODE_OP, GPIO_CONFIG_GP_OP_PP, GPIO_PIN13, GPIO_OP_SPEED_2);
+    GPIO_Init(&led);
+
+    while(1)
+	{
+		//Toggle PC13 Led Pin
+		GPIO_OpToggle(led.pGPIOx, led.GPIOx_PinConfig.PinNo);
+		wait(1);
+        Serialprint("LED at GPIOC Pin 13 is being toggled!\r\n\0");
+	}
+}
