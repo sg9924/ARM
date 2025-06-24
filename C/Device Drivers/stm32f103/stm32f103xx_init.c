efd1a7b750e562bef44f9985c332eb34e6365edc
sg9924 <sg9924@users.noreply.github.com>
2025-06-24 21:42:27 +0530

    stm32f103 | Board Init | Adding Timer Delay Functions
    Adding Timer Delay functions for milliseconds and microseconds using TIM2

--- C/Device Drivers/stm32f103/stm32f103xx_init.c
+++ C/Device Drivers/stm32f103/stm32f103xx_init.c
@@ -1,12 +1,16 @@
 #include"stm32f103xx_init.h"
 
 //USART2 for Serial Debugging
+//TIM2 for delays
 
 RCC_Handle R;
+TIM_Handle Delay;
 
 void board_init()
 {
+
     Serial_init();              // Initialize USART2 for Serial Print (Debugging)
+
     RCC_init(&R, RCC_CLK_HSI);  // Initialize RCC with HSI Clock
 
     if(RCC_Get_Clock_Source(&R) == RCC_CLK_HSE)
@@ -19,4 +23,26 @@ void board_init()
     Serialprint("[INFO]: HCLK is %d MHz\r\n", R.RCC_State.High_Clock/1000000);
     Serialprint("[INFO]: PCLK1 is %d MHz\r\n", R.RCC_State.P_Clock_1/1000000);
     Serialprint("[INFO]: PCLK2 is %d MHz\r\n", R.RCC_State.P_Clock_2/1000000);
+}
+
+
+void tim_delay_ms(uint16_t delay)
+{
+    //Configure Timer 2
+    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, (8000-1), delay, TIM_AR_PRELOAD_DISABLE);
+    TIM_Base_init(&Delay); //initialize
+    TIM_Base_Start(&Delay); //enable timer
+    TIM_Update_Event_Check(&Delay); //wait for update event flag
+    TIM_Base_Stop(&Delay); //disable timer
+}
+
+
+void tim_delay_us(uint16_t delay)
+{
+    //Configure Timer 2
+    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, (8-1), delay, TIM_AR_PRELOAD_DISABLE);
+    TIM_Base_init(&Delay); //initialize
+    TIM_Base_Start(&Delay); //enable timer
+    TIM_Update_Event_Check(&Delay); //wait for update event flag
+    TIM_Base_Stop(&Delay); //disable timer
 }
--- C/Device Drivers/stm32f103/stm32f103xx_init.h
+++ C/Device Drivers/stm32f103/stm32f103xx_init.h
@@ -1,6 +1,9 @@
 #include "stm32f103xx.h"
+#include "stm32f103xx_timer.h"
 #include "stm32f103xx_rcc.h"
 #include "stm32f103xx_serial.h"
 
 
 void board_init();
+void tim_delay_ms(uint16_t delay);
+void tim_delay_us(uint16_t delay);
