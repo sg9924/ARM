4a37f6d699d2130f65c52da7312dd22f702e2aea
sg9924 <sg9924@users.noreply.github.com>
2025-06-25 08:37:45 +0530

    stm32f103 | Seven Segment | Adding Functions
    1. Adding Pins Reset Function
    2. Adding more Arguments for the Init function
    3. Adding Display connection Type Definition

--- C/Device Drivers/stm32f103/seven_seg.h
+++ C/Device Drivers/stm32f103/seven_seg.h
@@ -3,12 +3,30 @@
 
 #include"stm32f103xx.h"
 #include"stm32f103xx_gpio.h"
-#include"seven_seg_5161AS.h"
 #include"gpio_7_seg_map.h"
+#include"seven_seg_5161AS.h"
+
+/*to be used later
+typedef struct
+{
+    GPIO_RegDef* Seg_Handle;
+    GPIO_RegDef* Digit_Handle;
+    uint8_t display_type;
+    uint8_t conn_type;
+    uint8_t selected_digit;
+
+}SevSeg_Handle;
+*/
+
+//Seven Segment Connection Type
+#define SEVEN_SEG_COMMON_CATHODE          0
+#define SEVEN_SEG_COMMON_ANODE            1
+
 
 //Functions
-void seven_seg_init(GPIO_Handle* pGPIOh);
-void seven_seg_display(GPIO_Handle* pGPIOh, uint8_t character);
+void seven_seg_init(GPIO_Handle* pGPIOh, GPIO_RegDef* GPIO_Port, uint8_t conn_type);
+void seven_seg_pins_reset(GPIO_Handle* pGPIOh, uint8_t conn_type);
+void seven_seg_display(GPIO_Handle* pGPIOh, uint8_t character, uint8_t conn_type);
 
 
 #endif /*SEVENSEG_H*/
