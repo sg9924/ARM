16a72e55390cbf417f59b0acb949d7e939d9e56b
sg9924 <sg9924@users.noreply.github.com>
2025-07-01 21:03:40 +0530

    stm32f103 | ADC | Add & Adjust API
    Add API's:
    - ADC_Sequence_Configure
    - ADC_Channel_init
    - ADC_Sequence_init
    - ADC_Reg_Seq_Config
    - ADC_No_Of_Conv
    
    Adjust API's:
    - ADC_GPIO_Config
    - ADC_Configure: add arg
    - ADC_init
    
    - Add ADC_Seq_Config, ADC_Seq_Handle structures
    - Add Definitions for ADC Events, ADC Sequence Nos, ADC Channel Nos & ADC Seq Types

--- C/Device Drivers/stm32f103/stm32f103xx_adc.c
+++ C/Device Drivers/stm32f103/stm32f103xx_adc.c
@@ -1,14 +1,16 @@
 #include"stm32f103xx_adc.h"
 
 
-
-
 static void ADC_GPIO_Config(ADC_Handle* pADCHandle)
 {
-    if(pADCHandle->pGPIOHandle->pGPIOx == GPIOA)
+    if(pADCHandle->pGPIOHandle->pGPIOx == GPIOA && pADCHandle->ADCx_Config.channel <= ADC_CHANNEL_7)
     {
-        // Configure for Analog Mode - Pin 6
-        GPIO_Config(pADCHandle->pGPIOHandle, pADCHandle->pGPIOHandle->pGPIOx, GPIO_MODE_IP, GPIO_CONFIG_ANALOG, GPIO_PIN6, GPIO_OP_SPEED_0);
+        GPIO_Config(pADCHandle->pGPIOHandle, pADCHandle->pGPIOHandle->pGPIOx, GPIO_MODE_IP, GPIO_CONFIG_ANALOG, pADCHandle->ADCx_Config.channel, GPIO_OP_SPEED_0);
+        GPIO_Init(pADCHandle->pGPIOHandle);
+    }
+    else if(pADCHandle->pGPIOHandle->pGPIOx == GPIOB && pADCHandle->ADCx_Config.channel > ADC_CHANNEL_7)
+    {
+        GPIO_Config(pADCHandle->pGPIOHandle, pADCHandle->pGPIOHandle->pGPIOx, GPIO_MODE_IP, GPIO_CONFIG_ANALOG, pADCHandle->ADCx_Config.channel, GPIO_OP_SPEED_0);
         GPIO_Init(pADCHandle->pGPIOHandle);
     }
 }
@@ -39,12 +41,23 @@ void ADC_PClk_init(ADC_Handle* pADCHandle, uint8_t mode)
 
 
 
-void ADC_Configure(ADC_Handle* pADCHandle, ADC_RegDef* ADCx, uint8_t mode, uint8_t event, uint8_t dma_mode)
+void ADC_Configure(ADC_Handle* pADCHandle, ADC_RegDef* ADCx, uint8_t mode, uint8_t scan, uint8_t event, uint8_t dma_mode)
 {
     pADCHandle->pADCx                        = ADCx;
     pADCHandle->ADCx_Config.mode             = mode;
+    pADCHandle->ADCx_Config.scan             = scan;
     pADCHandle->ADCx_Config.external_event   = event;
     pADCHandle->ADCx_Config.dma_mode         = dma_mode;
+}
+
+
+void ADC_Sequence_Configure(ADC_Seq_Handle* pADCSeqHandle, ADC_Handle* pADCHandle, uint8_t seq_type, uint8_t no_of_conv, uint8_t no_of_chnls, uint8_t* channel_list)
+{
+    pADCSeqHandle->pADCHandle                    = pADCHandle;
+    pADCSeqHandle->ADCx_Seq_Config.seq_type      = seq_type;
+    pADCSeqHandle->ADCx_Seq_Config.no_of_conv    = no_of_conv;
+    pADCSeqHandle->ADCx_Seq_Config.no_of_chnls   = no_of_chnls;
+    pADCSeqHandle->ADCx_Seq_Config.channel_list  = channel_list;
 }
 
 
@@ -66,7 +79,8 @@ void ADC_init(ADC_Handle* pADCHandle, GPIO_Handle* pGPIOHandle, GPIO_RegDef* GPI
         pADCHandle->pADCx->CR2 &= ~(1<<ADC_CR2_CONT);
     else if(pADCHandle->ADCx_Config.mode == ADC_MODE_CONT)
         pADCHandle->pADCx->CR2 |= 1<<ADC_CR2_CONT;
-    else if(pADCHandle->ADCx_Config.mode == ADC_MODE_SCAN)
+    
+    if(pADCHandle->ADCx_Config.scan == ADC_SCAN_ENABLE)
         pADCHandle->pADCx->CR1 |= 1<<ADC_CR1_SCAN;
 
     //ADC DMA
@@ -81,11 +95,85 @@ void ADC_init(ADC_Handle* pADCHandle, GPIO_Handle* pGPIOHandle, GPIO_RegDef* GPI
 }
 
 
+void ADC_Channel_init(ADC_Handle* pADCHandle, uint8_t channel)
+{
+    pADCHandle->ADCx_Config.channel = channel;
+    //Set Channel Number in Regular Sequence
+    ADC_Reg_Seq_Config(pADCHandle->pADCx,pADCHandle->ADCx_Config.channel, ADC_SEQ_NO_1);
+}
+
+
+
+
+void ADC_Sequence_init(ADC_Seq_Handle* pADCSeqHandle, GPIO_Handle* pGPIOHandle, GPIO_RegDef* GPIOx)
+{
+    uint8_t no_of_chnls = pADCSeqHandle->ADCx_Seq_Config.no_of_chnls;
+
+    //initialize ADC
+    ADC_init(pADCSeqHandle->pADCHandle, pGPIOHandle, GPIOx);
+
+    //set no of conversions
+    ADC_No_Of_Conv(pADCSeqHandle->pADCHandle->pADCx, pADCSeqHandle->ADCx_Seq_Config.no_of_conv);
+
+    //set channel numbers in sequence registers according to the order of the channels
+    while(--no_of_chnls >= 0)
+    {
+        ADC_Reg_Seq_Config(pADCSeqHandle->pADCHandle->pADCx, pADCSeqHandle->ADCx_Seq_Config.channel_list[no_of_chnls], (no_of_chnls+1));
+    }
+}
+
+
+void ADC_IT_Config()
+{
+    
+}
+
+
+void ADC_Reg_Seq_Config(ADC_RegDef* pADCx, uint8_t channel, uint8_t seq_no)
+{
+    uint8_t SQR_Reg_bit_offset = (((seq_no%16)-1)*5)%30;
+
+    if(seq_no>=1 && seq_no<=6)
+    {
+        if(channel == ADC_CHANNEL_0)
+            pADCx->SQR3 &= ~(0x1F << SQR_Reg_bit_offset);
+        else if(channel >= ADC_CHANNEL_1)
+            pADCx->SQR3 |= (channel) << SQR_Reg_bit_offset;
+    }
+    else if(seq_no>=7 && seq_no<=12)
+    {
+        if(channel == ADC_CHANNEL_0)
+            pADCx->SQR2 &= ~(0x1F  << SQR_Reg_bit_offset);
+        else if(channel >= ADC_CHANNEL_1)
+            pADCx->SQR2 |= (channel) << SQR_Reg_bit_offset;
+    }
+    else if(seq_no>=13 && seq_no<=16)
+    {
+        if(channel == ADC_CHANNEL_0)
+            pADCx->SQR1 &= ~(0x1F  << SQR_Reg_bit_offset);
+        else if(channel >= ADC_CHANNEL_1)
+            pADCx->SQR1 |= (channel) << SQR_Reg_bit_offset;
+    }
+}
+
+
+
+
+void ADC_No_Of_Conv(ADC_RegDef* pADCx, uint8_t no_of_conversions)
+{
+    if(no_of_conversions != 1)
+        pADCx->SQR1 |= (no_of_conversions - 1) << ADC_SQR1_L;
+    else if(no_of_conversions == 1)
+        pADCx->SQR1 &= ~(1<<ADC_SQR1_L);
+}
+
+
+
 static void _ADC_Start()
 {
     //Enable ADC to wake from sleep
     ADC1_ENABLE();
-    //!!delay should be added here!!
+    //!!delay should be provided here!!
     //Enable ADC after Wake
     ADC1_ENABLE();
 }
@@ -106,7 +194,7 @@ uint16_t ADC_Start(ADC_Handle* pADCHandle)
 }
 
 
-
+//needs to be expanded to include DMA here
 void ADC_DMA_Start()
 {
     _ADC_Start();
--- C/Device Drivers/stm32f103/stm32f103xx_adc.h
+++ C/Device Drivers/stm32f103/stm32f103xx_adc.h
@@ -12,12 +12,22 @@
 typedef struct
 {
     uint8_t   mode;
+    uint8_t   scan;
     uint8_t   external_event;
     uint8_t   dma_mode;
-    uint8_t   channel_no;
+    uint8_t   channel;
     uint8_t   resolution;
+    uint8_t   smaple_time;
     uint16_t* pbuffer;
 }ADC_Config;
+
+typedef struct
+{
+    uint8_t seq_type;
+    uint8_t no_of_conv;
+    uint8_t no_of_chnls;
+    uint8_t* channel_list;
+}ADC_Seq_Config;
 
 
 
@@ -28,6 +38,12 @@ typedef struct
     GPIO_Handle* pGPIOHandle;
     ADC_Config   ADCx_Config;
 }ADC_Handle;
+
+typedef struct
+{
+    ADC_Handle* pADCHandle;
+    ADC_Seq_Config ADCx_Seq_Config;
+}ADC_Seq_Handle;
 
 
 
@@ -115,6 +131,23 @@ typedef struct
 #define ADC_SMPR2_SMP9              27
 
 
+//ADC_SQR1 Bit Field Definitions
+#define ADC_SQR1_SQ13               0
+#define ADC_SQR1_SQ14               5
+#define ADC_SQR1_SQ15               10
+#define ADC_SQR1_SQ16               15
+#define ADC_SQR1_L                  20
+
+//ADC_SQR3 Bit Field Definitions
+#define ADC_SQR3_SQ1                0
+#define ADC_SQR3_SQ2                5
+#define ADC_SQR3_SQ3                10
+#define ADC_SQR3_SQ4                15
+#define ADC_SQR3_SQ5                20
+#define ADC_SQR3_SQ6                25
+
+
+
 //ADC_DR
 #define ADC_DR_DATA                 0
 #define ADC_DR_ADC2DATA             16
@@ -127,11 +160,65 @@ typedef struct
 //ADC Conversion Modes
 #define ADC_MODE_SINGLE             0
 #define ADC_MODE_CONT               1
-#define ADC_MODE_SCAN               2
+
+//ADC Scan
+#define ADC_SCAN_DISABLE            0
+#define ADC_SCAN_ENABLE             1
 
 
-//ADC Trigger
-#define ADC_EVENT_SWSTART           7
+//ADC Events Trigger
+#define ADC1_2_EVENT_TIM1_CC1             0
+#define ADC1_2_EVENT_TIM1_CC2             1
+#define ADC1_2_EVENT_TIM1_CC3             2
+#define ADC1_2_EVENT_TIM2_CC2             3
+#define ADC1_2_EVENT_TIM3_TRGO            4
+#define ADC1_2_EVENT_TIM4_CC4             5
+#define ADC1_2_EVENT_EXTI_11_TIM8_TRGO    6
+#define ADC1_2_EVENT_SWSTART              7
+
+#define ADC_EVENT_TIM3_CC1                0
+#define ADC_EVENT_TIM2_CC3                1
+#define ADC_EVENT_TIM1_CC3                2
+#define ADC_EVENT_TI82_CC1                3
+#define ADC_EVENT_TIM8_TRGO               4
+#define ADC_EVENT_TIM5_CC1                5
+#define ADC_EVENT_TIM5_CC3                6
+#define ADC_EVENT_SWSTART                 7
+
+//ADC Channels
+#define ADC_CHANNEL_0                     GPIO_PIN0
+#define ADC_CHANNEL_1                     GPIO_PIN1
+#define ADC_CHANNEL_2                     GPIO_PIN2
+#define ADC_CHANNEL_3                     GPIO_PIN3
+#define ADC_CHANNEL_4                     GPIO_PIN4
+#define ADC_CHANNEL_5                     GPIO_PIN5
+#define ADC_CHANNEL_6                     GPIO_PIN6
+#define ADC_CHANNEL_7                     GPIO_PIN7
+#define ADC_CHANNEL_8                     GPIO_PIN0
+#define ADC_CHANNEL_9                     GPIO_PIN1
+
+//ADC Sequence numbers
+#define ADC_SEQ_NO_1                      1
+#define ADC_SEQ_NO_2                      2
+#define ADC_SEQ_NO_3                      3
+#define ADC_SEQ_NO_4                      4
+#define ADC_SEQ_NO_5                      5
+#define ADC_SEQ_NO_6                      6
+#define ADC_SEQ_NO_7                      7
+#define ADC_SEQ_NO_8                      8
+#define ADC_SEQ_NO_9                      9
+#define ADC_SEQ_NO_10                     10
+#define ADC_SEQ_NO_11                     11
+#define ADC_SEQ_NO_12                     12
+#define ADC_SEQ_NO_13                     13
+#define ADC_SEQ_NO_14                     14
+#define ADC_SEQ_NO_15                     15
+#define ADC_SEQ_NO_16                     16
+
+//ADC Sequence Type
+#define ADC_SEQ_TYPE_REGULAR              0
+//#define ADC_SEQ_TYPE_INJECTED
+
 
 
 //ADC Peripheral Clock Enable
@@ -165,11 +252,22 @@ typedef struct
 
 
 //Function Declarations
+//Config and Init
 void ADC_PClk_init(ADC_Handle* pADCHandle, uint8_t mode);
-void ADC_Configure(ADC_Handle* pADCHandle, ADC_RegDef* ADCx, uint8_t mode, uint8_t event, uint8_t dma_mode);
+void ADC_Configure(ADC_Handle* pADCHandle, ADC_RegDef* ADCx, uint8_t mode, uint8_t scan, uint8_t event, uint8_t dma_mode);
 void ADC_init(ADC_Handle* pADCHandle, GPIO_Handle* pGPIOHandle, GPIO_RegDef* GPIOx);
+void ADC_Channel_init(ADC_Handle* pADCHandle, uint8_t channel);
+void ADC_Reg_Seq_Config(ADC_RegDef* pADCx, uint8_t channel, uint8_t seq_no);
+void ADC_No_Of_Conv(ADC_RegDef* pADCx, uint8_t no_of_conversions);
+
+//ADC Start
 uint16_t ADC_Start(ADC_Handle* pADCHandle);
 void ADC_DMA_Start();
+
+//Interrupts
+void ADC_IT_Config();
+
+void ADC_Sequence_init(ADC_Seq_Handle* pADCSeqHandle, GPIO_Handle* pGPIOHandle, GPIO_RegDef* GPIOx);
 
 
 
