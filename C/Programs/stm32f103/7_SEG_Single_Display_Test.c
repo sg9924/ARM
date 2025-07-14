262e913b4f62a5b83d7ecf94f21215ff1923b5aa
sg9924 <sg9924@users.noreply.github.com>
2025-07-14 21:20:31 +0530

    Update 7_SEG_Single_Display_Test.c
    Modified program for the new drivers

--- C/Programs/stm32f103/7_SEG_Single_Display_Test.c
+++ C/Programs/stm32f103/7_SEG_Single_Display_Test.c
@@ -1,22 +1,27 @@
 #include<stdint.h>
 #include"stm32f103xx_init.h"
-#include"seven_seg.h"
+#include"seg_display.h" //5161AS config header should be included inside this
 
+SevSeg_Handle S;
+GPIO_Handle G;
+
+segments_t seg_pins = {GPIO_PIN7, GPIO_PIN9, GPIO_PIN0, GPIO_PIN5, GPIO_PIN10, GPIO_PIN8, GPIO_PIN4, GPIO_PIN1};
 
 int main()
 {
 	board_init();
 
-	//GPIO Handle
-	GPIO_Handle d1;
-	seven_seg_init(&d1);
+	seg_pin_configure(&S,&seg_pins, GPIOA);
+    seg_init(&S, &G);
+
 
 	char a[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	
 	int i=0;
+
+	//display all numbers from 0-9
 	while(1)
-	{		
-		seven_seg_display(&d1, a[i]);
+	{	
+		seg_display(S.Seg_GPIO_Port, &seg_pins, a[i]);	
 		tim_delay_ms(1000);
 
 		i=(i+1)%10;
