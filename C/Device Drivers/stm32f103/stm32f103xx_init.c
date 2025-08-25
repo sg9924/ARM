d4949be14549a2ca744e6f16a0e3e54b0b9456cd
sg9924 <sg9924@users.noreply.github.com>
2025-08-25 12:23:52 +0530

    stm32f103 | Board Init | Add API's
    - Add API's related to CPUID
    - Some formatting in Serial Print

--- C/Device Drivers/stm32f103/stm32f103xx_init.c
+++ C/Device Drivers/stm32f103/stm32f103xx_init.c
@@ -1,4 +1,9 @@
-#include"stm32f103xx_init.h"
+#include "stm32f103xx_init.h"
+#include "stm32f103xx.h"
+#include "stm32f103xx_core.h"
+#include "stm32f103xx_timer.h"
+#include "stm32f103xx_rcc.h"
+#include "stm32f103xx_serial.h"
 
 //USART2 for Serial Debugging
 //TIM2 for delays
@@ -8,18 +13,39 @@ TIM_Handle Delay;
 
 void board_init()
 {
-
     Serial_init();              // Initialize USART2 for Serial Print (Debugging)
 
+    display_cpu_info();
     RCC_init(&R, RCC_CLK_HSI);  // Initialize RCC with HSI Clock
+    display_clk_src();
+    display_clk_freqs();
+    
+}
 
+
+void display_cpu_info()
+{
+    Serialprint("\r\nProcessor Info:\r\n");
+    Serialprint("Implementer: %x\r\n", GET_IMPLEMENTER());
+    Serialprint("Variant: %x\r\n", GET_VARIANT());
+    Serialprint("Part No: %x\r\n", GET_PARTNO());
+    Serialprint("Revision: %x\r\n", GET_REVISION());
+}
+
+
+void display_clk_src()
+{
     if(RCC_Get_Clock_Source(&R) == RCC_CLK_HSE)
-        Serialprint("[INFO]: Clock Source is HSE\r\n");
+        Serialprint("\r\n[INFO]: Clock Source is HSE\r\n");
     else if(RCC_Get_Clock_Source(&R)  == RCC_CLK_HSI)
-        Serialprint("[INFO]: Clock Source is HSI\r\n");
+        Serialprint("\r\n[INFO]: Clock Source is HSI\r\n");
     else if(RCC_Get_Clock_Source(&R)  == RCC_CLK_PLL)
-        Serialprint("[INFO]: Clock Source is PLL\r\n");
+        Serialprint("\r\n[INFO]: Clock Source is PLL\r\n");
+}
 
+
+void display_clk_freqs()
+{
     Serialprint("[INFO]: HCLK is %d MHz\r\n", R.RCC_State.High_Clock/1000000);
     Serialprint("[INFO]: PCLK1 is %d MHz\r\n", R.RCC_State.P_Clock_1/1000000);
     Serialprint("[INFO]: PCLK2 is %d MHz\r\n", R.RCC_State.P_Clock_2/1000000);
--- C/Device Drivers/stm32f103/stm32f103xx_init.h
+++ C/Device Drivers/stm32f103/stm32f103xx_init.h
@@ -1,9 +1,16 @@
-#include "stm32f103xx.h"
-#include "stm32f103xx_timer.h"
-#include "stm32f103xx_rcc.h"
-#include "stm32f103xx_serial.h"
+#ifndef INC_STM32F103XX_INIT_H
+#define INC_STM32F103XX_INIT_H
+
+#include<stdint.h>
 
 
 void board_init();
 void tim_delay_ms(uint16_t delay);
 void tim_delay_us(uint16_t delay);
+
+void display_cpu_info();
+void display_clk_src();
+void display_clk_freqs();
+
+
+#endif
