02f136512857de7c63d9c199d3334f542b76a7a4
sg9924 <sg9924@users.noreply.github.com>
2026-02-07 19:42:26 +0530

    stm32f103 | Board Init | Improvements
    Improvements
    Added more LED API's
    Added clock init
    Modified TIM2 Init

--- C/Device_Drivers/stm32f103/inc/stm32f103xx_init.h
+++ C/Device_Drivers/stm32f103/inc/stm32f103xx_init.h
@@ -6,13 +6,14 @@
 
 void board_init();
 void led_init();
+void led_reset();
 void led_toggle();
+void clock_init();
 void tim_delay_ms(uint16_t delay);
 void tim_delay_us(uint16_t delay);
 
 void display_cpu_info();
-void display_clk_src();
-void display_clk_freqs();
+void display_clk_info();
 
 
 #endif
--- C/Device_Drivers/stm32f103/src/stm32f103xx_init.c
+++ C/Device_Drivers/stm32f103/src/stm32f103xx_init.c
@@ -8,18 +8,19 @@
 //USART2 for Serial Debugging
 //TIM2 for delays
 
-RCC_Handle R;
+//RCC_Handle R;
 TIM_Handle Delay;
 GPIO_Handle LED;
+extern uint32_t sysclock;
 
 void board_init()
 {
-    Serial_init();              // Initialize USART2 for Serial Print (Debugging)
-
+    clock_init();  //SYSCLK: PLL with HSE at 72MHz
+    Serial_init(); //Initialize USART2 for Serial Print (Debugging)
+    led_init();    //Initialize built in LED at PC13
+    led_reset();   //switch off led
     display_cpu_info();
-    RCC_init(&R, RCC_CLK_HSI);  // Initialize RCC with HSI Clock
-    display_clk_src();
-    display_clk_freqs();
+    display_clk_info();
     
 }
 
@@ -30,6 +31,12 @@ void led_init()
     GPIO_Init(&LED);
 }
 
+void led_reset()
+{
+    //GPIO_Bit_Reset(&LED, GPIO_PIN13);
+    GPIO_Bit_Set(&LED, GPIO_PIN13);
+    Serialprintln("User LED has been initialized and reset", INFO);
+}
 
 void led_toggle()
 {
@@ -37,9 +44,17 @@ void led_toggle()
 }
 
 
+void clock_init()
+{
+    RCC_Config_PLL(PLL_CLK_SRC_HSE, PLL_MUL_9, PLL_HSE_DIV_NONE);
+    RCC_init(RCC_CLK_HSE, SYSCLK_PLL, AHB_PRESCALAR_1, APB1_PRESCALAR_2, APB2_PRESCALAR_1);
+}
+
+
 void display_cpu_info()
 {
-    Serialprintln("Processor Info:", INFO);
+    SERIAL_NL();
+    Serialprintln("Processor Info:", NONE);
     Serialprintln("Implementer: %x", INFO, GET_IMPLEMENTER());
     Serialprintln("Variant: %x", INFO, GET_VARIANT());
     Serialprintln("Part No: %x", INFO, GET_PARTNO());
@@ -47,26 +62,23 @@ void display_cpu_info()
 }
 
 
-void display_clk_src()
+void display_clk_info()
 {
-    if(RCC_Get_Clock_Source(&R) == RCC_CLK_HSE)       Serialprintln("Clock Source is HSE", INFO);
-    else if(RCC_Get_Clock_Source(&R)  == RCC_CLK_HSI) Serialprintln("Clock Source is HSI", INFO);
-    else if(RCC_Get_Clock_Source(&R)  == RCC_CLK_PLL) Serialprintln("Clock Source is PLL", INFO);
-}
+    SERIAL_NL();
+    Serialprintln("Clock Info:", NONE);
+    if(RCC_Get_Clock_Source() == RCC_CLK_HSE)       Serialprintln("Clock Source is HSE", INFO);
+    else if(RCC_Get_Clock_Source()  == RCC_CLK_HSI) Serialprintln("Clock Source is HSI", INFO);
 
-
-void display_clk_freqs()
-{
-    Serialprintln("HCLK is %d MHz", INFO, R.RCC_State.High_Clock/1000000);
-    Serialprintln("PCLK1 is %d MHz", INFO, R.RCC_State.P_Clock_1/1000000);
-    Serialprintln("PCLK2 is %d MHz", INFO, R.RCC_State.P_Clock_2/1000000);
+    Serialprintln("HCLK is %d MHz", INFO, RCC_Get_HCLK()/1000000);
+    Serialprintln("PCLK1 is %d MHz", INFO, RCC_Get_PCLK1()/1000000);
+    Serialprintln("PCLK2 is %d MHz", INFO, RCC_Get_PCLK2()/1000000);
 }
 
 
 void tim_delay_ms(uint16_t delay)
 {
     //Configure Timer 2
-    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, (8000-1), delay, TIM_AR_PRELOAD_ENABLE);
+    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, ((sysclock/10000)-1), (delay*10)-1, TIM_AR_PRELOAD_ENABLE);
     TIM_Count_Reset(&Delay); //reset count
     TIM_Base_init(&Delay); //initialize
     TIM_Base_Start(&Delay); //enable timer
@@ -78,7 +90,7 @@ void tim_delay_ms(uint16_t delay)
 void tim_delay_us(uint16_t delay)
 {
     //Configure Timer 2
-    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, (8-1), delay, TIM_AR_PRELOAD_ENABLE);
+    TIM_Base_Configure(&Delay, TIM2, TIM_COUNT_DIR_UP, ((sysclock/1000000)-1), delay-1, TIM_AR_PRELOAD_ENABLE);
     TIM_Base_init(&Delay); //initialize
     TIM_Base_Start(&Delay); //enable timer
     TIM_Update_Event_Check(&Delay); //wait for update event flag
